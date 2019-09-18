25da27e8edbf27bf9f7d83399301e29e81f0bdf1
jackson ford <jfordnose64@gmail.com>
2019-09-18 13:33:04 -0400

    added post call

--- Controllers/ItemControllers.cs
+++ Controllers/ItemControllers.cs
@@ -4,23 +4,47 @@ using inventory.Models;
 using System;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections;
+using inventory;
 
 namespace inventory.Controllers
 {
   [Route("api/[controller]")]
   [ApiController]
+
   public class inventoryController : ControllerBase
   {
     [HttpGet]
-    public ActionResult<string> Sum(string s)
+    public ActionResult<IEnumerable> GetAllItems()
     {
-      return "Wow";
+      var context = new DatabaseContext();
+      var theThing = context.Items.OrderByDescending(item => item.Id);
+      return theThing.ToList();
     }
 
-    // [HttpPost]
-    // public ActionResult<string> CreateItem()
-    // {
+    [HttpGet("{Id}")]
+    public ActionResult GetOneItem(int id)
+    {
+      var context = new DatabaseContext();
+      var oneItem = context.Items.FirstOrDefault(i => i.Id == id);
+      if (oneItem == null)
+      {
+        return NotFound();
+      }
+      else
+      {
+        return Ok(oneItem);
+      }
+    }
 
-    // }
+    [HttpPost]
+    public ActionResult<Item> CreateEntry([FromBody] Item entry)
+    {
+      var context = new DatabaseContext();
+      context.Items.Add(entry);
+      context.SaveChanges();
+      return entry;
+    }
+
   }
 }
